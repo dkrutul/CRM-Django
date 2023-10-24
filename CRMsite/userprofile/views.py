4fc2364f7d4027735286384c570838dd54315ea7
dkrutul <dawtul01@gmail.com>
2023-10-24 12:22:19 +0200

    Log in and logout structure, dashboard view

---
+++ CRMsite/userprofile/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+from .models import Userprofile
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            Userprofile.objects.create(user=user)
+
+            return redirect('/log-in/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html', {
+        'form': form
+    })
